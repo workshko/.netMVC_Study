84ba668193f1b7d96904f4c03c7d96eab0195106
shko <work.shko@gmail.com>
2020-04-04 09:27:08 +0900

    1. asp.net MVC 빈 프로젝트 생성 ( 솔루션 및 프로젝트 동일구조로 체크)

---
+++ dropzone_sample/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace dropzone_sample
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
