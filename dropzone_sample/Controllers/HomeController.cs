5aaa57e7de053072646aa751bcec5a56535f6a95
shko <work.shko@gmail.com>
2020-04-04 10:29:35 +0900

    1. 홈컨트롤러 추가 2. Normalize.css , reset.css 추가 ( 둘다 css를 초기화하는것에는 변함 없음, 그러나 Normalize의 경우에는 각각의 기본 속성을 살림, reset의 경우에는 모든 css 속성 초기화) 3. Site.css 추가 및 다른 css import 4. dropzone 관련 css 및 js 추가 5. Home컨트롤러 추가

---
+++ dropzone_sample/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dropzone_sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// 1. dropzone에서는 파일을 여러개 올리더라도 옵션이 'uploadMultiple = false' 설정되어있으므로, 파일 올린 수만큼 해당 함수 호출
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult UploadFiles() 
+        {
+            System.Diagnostics.Debug.Print(Request.Files.Count.ToString());
+
+            return View();
+        }
+
+
+    }
+}
